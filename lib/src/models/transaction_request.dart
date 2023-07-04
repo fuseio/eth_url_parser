59760e8767e9e1aac1e22288a46be28f7059880d
Lior Agnin <lior.agnin@gmail.com>
2023-07-04 16:05:25 +0300

    Update deps

--- lib/src/models/transaction_request.dart
+++ lib/src/models/transaction_request.dart
@@ -3,8 +3,31 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'transaction_request.freezed.dart';
 part 'transaction_request.g.dart';
 
+/// Represents a request to perform a transaction on a blockchain.
+///
+/// This class encapsulates the necessary information to perform a transaction
+/// on a blockchain. It is designed to be flexible and can be used for different
+/// blockchain schemes.
+///
+/// Example:
+/// ```dart
+/// var request = TransactionRequest(
+///   targetAddress: '0x123...',
+///   functionName: 'transfer',
+///   parameters: {'value': '1.23'},
+///   chainId: 122,
+/// );
+/// ```
 @freezed
 class TransactionRequest with _$TransactionRequest {
+  /// Creates a new transaction request.
+  ///
+  /// [scheme] represents the blockchain scheme, default is 'ethereum'.
+  /// [targetAddress] is the blockchain address the transaction is directed to.
+  /// [functionName] is the name of the function to be called in the smart contract (optional).
+  /// [prefix] can be used to add any prefix data needed for the transaction (optional).
+  /// [chainId] is the ID of the blockchain network (optional).
+  /// [parameters] is a map of parameters to be sent in the transaction (optional).
   factory TransactionRequest({
     @Default('ethereum') String scheme,
     required String targetAddress,
@@ -14,6 +37,9 @@ class TransactionRequest with _$TransactionRequest {
     @Default({}) Map<String, dynamic> parameters,
   }) = _TransactionRequest;
 
+  /// Creates a new transaction request from a JSON object.
+  ///
+  /// [json] is a map containing the JSON data.
   factory TransactionRequest.fromJson(Map<String, dynamic> json) =>
       _$TransactionRequestFromJson(json);
 }
